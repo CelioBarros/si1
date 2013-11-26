66d85b2ac2a8487b15f0e9bddfa72a28c732fc51
CelioBarros <celio.filho@ccc.ufcg.edu.br>
2013-11-26 00:09:37 -0200

    lab2 terceira questao, Redering the first page

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -1,26 +1,27 @@
 package controllers;
 
-import play.*;
+import play.data.*;
 import play.mvc.*;
-
-import views.html.*;
+import models.*;
 
 public class Application extends Controller {
+	
+	static Form<Task> taskForm = Form.form(Task.class);
 
     public static Result index() {
     	return redirect(routes.Application.tasks());
     }
 
     public static Result tasks() {
-        return TODO;
-      }
+     return ok(views.html.index.render(Task.all(), taskForm));
+    }
       
     public static Result newTask() {
         return TODO;
-      }
+    }
       
     public static Result deleteTask(Long id) {
         return TODO;
-      }
+    }
 
 }
--- lab2/todolist/app/models/Task.java
+++ lab2/todolist/app/models/Task.java
@@ -1,11 +1,15 @@
 package models;
 
 import java.util.*;
+import play.data.validation.Constraints.*;
+
 
 public class Task {
     
-  public Long id;
-  public String label;
+	public Long id;
+	  
+	@Required
+	public String label;
   
   public static List<Task> all() {
     return new ArrayList<Task>();
