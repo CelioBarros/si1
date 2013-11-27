a55c1f2021178b095261f4e01eea67878e6ef05d
CelioBarros <celio.filho@ccc.ufcg.edu.br>
2013-11-26 23:56:00 -0200

    lab2 parte 5

--- lab2/todolist/app/models/Task.java
+++ lab2/todolist/app/models/Task.java
@@ -10,23 +10,72 @@ import javax.persistence.*;
 public class Task extends Model {
 
 	@Id
-	public Long id;
+	private Long id;
 
 	@Required
-	public String label;
+    private String descricao;
+	
+	@Required
+	private String projeto;
+	
+	@Required
+	private String tarefa;
+	
+	@Required
+	private int prioridade;
 
 	public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
 
 	public static List<Task> all() {
 		return find.all();
 	}
-
+	
 	public static void create(Task task) {
 		task.save();
 	}
-
+	
 	public static void delete(Long id) {
 		find.ref(id).delete();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getProjeto() {
+		return this.projeto;
+	}
+
+	public void setProjeto(String projeto) {
+		this.projeto = projeto;
+	}
+
+	public String getTarefa() {
+		return this.tarefa;
+	}
+
+	public void setTarefa(String tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public int getPrioridade() {
+		return this.prioridade;
+	}
+
+	public void setPrioridade(int prioridade) {
+		this.prioridade = prioridade;
 	}
 
 }
